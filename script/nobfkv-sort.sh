ee88582bfe4f860d1b3223f472deb990783c335c
Sjur Moshagen <sjurnm@mac.com>
2010-03-02 09:23:02 +0000

    Shell script for sorting each input file using the sorting xsl, one file at a time. The sorted files will replace the old ones.

---
+++ script/nobfkv-sort.sh
@@ -0,0 +1,11 @@
+#!sh
+#
+# Shell script to run the sort xsl over a number of xml files, one file at a
+# time. The calling of the xsl script is very fragile, and should only be used
+# from the make file in the dir above.
+
+for i in $@
+do
+xsltproc script/nobfkv-sort.xsl $i > $i.new
+mv -f $i.new $i
+done
