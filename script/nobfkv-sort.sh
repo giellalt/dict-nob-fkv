4e2f7fededba3b8dbb8367e906ee294867ee3551
Sjur Moshagen <sjurnm@mac.com>
2010-03-02 09:55:36 +0000

    • corrected the shell reference • work-around for the bad indentation caused by the xsl sort script. With this change, only the real differences will show up in the svn diff.

--- script/nobfkv-sort.sh
+++ script/nobfkv-sort.sh
@@ -1,4 +1,4 @@
-#!sh
+#!/bin/bash
 #
 # Shell script to run the sort xsl over a number of xml files, one file at a
 # time. The calling of the xsl script is very fragile, and should only be used
@@ -6,6 +6,6 @@
 
 for i in $@
 do
-xsltproc script/nobfkv-sort.xsl $i > $i.new
+xsltproc script/nobfkv-sort.xsl $i | sed 's/^  </   </' > $i.new
 mv -f $i.new $i
 done
