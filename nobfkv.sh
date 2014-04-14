de5352151efd4117f396694d4a908b5b9fa3d98e
Trond Trosterud <trond.trosterud@uit.no>
2014-04-14 09:11:44 +0000

    Updated and corrected shell script.

--- nobfkv.sh
+++ nobfkv.sh
@@ -5,13 +5,18 @@
 # Førebels er det berre eit shellscript.
 
 # Kommando for å lage nobfkv.fst
-echo "skriv:"
+echo 
+echo "Etter at dette scriptet er ferdig står du i xfst med promten"
+echo "xfst[1]"
+echo ""
+echo "Gjer då dette:"
 echo "invert"
 echo "save bin/nobfkv.fst"
+echo "quit"
 echo ""
 
 echo "LEXICON Root" > bin/nobfkv.lexc
-cat src/*_nobfkv.xml | tr '\n' '™' | sed 's/<e>/£/g;'| tr '£' '\n'| grep -v 'xml version'|sed 's/<re>[^>]*>//g;'|tr '<' '>'| cut -d">" -f3,11| tr ' ' '_'| tr '>' ':'| grep -v '__'|sed 's/$/ # ;/g' >> bin/nobfkv.lexc
+cat src/*_nobfkv.xml | tr '\n' '™' | sed 's/<e/£/g;'| tr '£' '\n'| grep -v 'xml version'|sed 's/<re>[^>]*>//g;'|tr '<' '>'| cut -d">" -f6,16| tr ' ' '_'| tr '>' ':'| grep -v '__'|sed 's/$/ # ;/g' >> bin/nobfkv.lexc
  
 xfst -e "read lexc < bin/nobfkv.lexc"
 
